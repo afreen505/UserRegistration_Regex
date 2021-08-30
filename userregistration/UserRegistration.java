c272d736dbfb1e28af842c3d52c277787e941e7b
afreen505 <afreen.fathimaa9876@gmail.com>
2021-08-30 21:49:24 +0530

    Modified: Uc4 added and executed

--- userregistration/UserRegistration.java
+++ userregistration/UserRegistration.java
@@ -7,54 +7,22 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     Scanner scanner = new Scanner(System.in);
 
-    private void validFirstName() {
-        System.out.println("Enter the First name: ");
-        String name = scanner.next();
-        String regex = "^[A-Z][a-z]{2,}";
+    private void validMobileNo() {
+        System.out.println("Enter the Mobile number: ");
+        String mobileNumber = scanner.nextLine();
+        String regex = "[0-9]{2}\\s{1}[0-9]{10}";
         Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(name);
-        boolean b = m.matches();
-        if (b) {
-            System.out.println("First Name of the user is valid.");
-        } else {
-            System.out.println("First Name of the user is invalid.");
-        }
-    }
-
-    private void validLastName() {
-        System.out.println("Enter the Last name: ");
-        String name = scanner.next();
-        String regex = "^[A-Z][a-z]{2,}";
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(name);
-        boolean b = m.matches();
-        if (b) {
-            System.out.println("Last Name of the user is valid.");
-        } else {
-            System.out.println("Last Name of the user is invalid.");
-        }
-    }
-
-    private void validEmail() {
-        System.out.println("Enter the Email Id: ");
-        String emailId = scanner.next();
-        String regex = "^[a-z0-9.+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$";
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(emailId);
+        Matcher m = p.matcher(mobileNumber);
         boolean b = m.matches();
         if (b)
-            System.out.println("Email Id of the user is valid.");
+            System.out.println("Mobile number of the user is valid.");
         else
-            System.out.println("Email Id of the user is invalid.");
+            System.out.println("Mobile number of the user is invalid.");
 
     }
-
 
     public static void main(String[] args) {
         UserRegistration user = new UserRegistration();
-        user.validFirstName();
-        user.validLastName();
-        user.validEmail();
+        user.validMobileNo();
     }
-
 }
