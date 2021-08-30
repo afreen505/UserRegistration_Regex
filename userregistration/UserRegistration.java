f2f542d16ecc4bf57d8c10685d7989f46f55c128
afreen505 <afreen.fathimaa9876@gmail.com>
2021-08-30 16:37:05 +0530

    Modified: Uc2 Executed Successfully

--- userregistration/UserRegistration.java
+++ userregistration/UserRegistration.java
@@ -6,7 +6,7 @@ public class UserRegistration {
     Scanner scanner = new Scanner(System.in);
 
     private void validFirstName() {
-        System.out.println("Enter the first name: ");
+        System.out.println("Enter the First name: ");
         String name = scanner.next();
         String regex = "^[A-Z][a-z]{3,}";
         if (name.matches(regex)) {
@@ -16,9 +16,21 @@ public class UserRegistration {
         }
     }
 
+    private void validLastName() {
+        System.out.println("Enter the Last name: ");
+        String name = scanner.next();
+        String regex = "^[A-Z][a-z]{3,}";
+        if (name.matches(regex)) {
+            System.out.println("Last Name of the user is valid.");
+        } else {
+            System.out.println("Last Name of the user is invalid.");
+        }
+    }
+
     public static void main(String[] args) {
         UserRegistration user = new UserRegistration();
         user.validFirstName();
+        user.validLastName();
     }
 
 }
