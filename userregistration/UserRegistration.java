19a6668afa2b09de1dfe6c07bd7919d5d3fb7b7f
afreen505 <afreen.fathimaa9876@gmail.com>
2021-08-30 21:52:46 +0530

    Modified: Uc5 added and executed

--- userregistration/UserRegistration.java
+++ userregistration/UserRegistration.java
@@ -7,6 +7,51 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     Scanner scanner = new Scanner(System.in);
 
+    private void validFirstName() {
+        System.out.println("Enter the First name: ");
+        String name = scanner.next();
+        String regex = "^[A-Z][a-z]{2,}";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(name);
+        boolean b = m.matches();
+        if (b) {
+            System.out.println("First Name of the user is valid.");
+        } else {
+            System.out.println("First Name of the user is invalid.");
+        }
+    }
+
+    private void validLastName() {
+        System.out.println("Enter the Last name: ");
+        String name = scanner.next();
+        String regex = "^[A-Z][a-z]{2,}";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(name);
+        boolean b = m.matches();
+        if (b) {
+            System.out.println("Last Name of the user is valid.");
+        } else {
+            System.out.println("Last Name of the user is invalid.");
+        }
+    }
+
+    /**
+     * UC-3 This method is validating the email id of the user is correct or not.
+     */
+    private void validEmail() {
+        System.out.println("Enter the Email Id: ");
+        String emailId = scanner.next();
+        String regex = "^[a-z0-9.+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(emailId);
+        boolean b = m.matches();
+        if (b)
+            System.out.println("Email Id of the user is valid.");
+        else
+            System.out.println("Email Id of the user is invalid.");
+
+    }
+
     private void validMobileNo() {
         System.out.println("Enter the Mobile number: ");
         String mobileNumber = scanner.nextLine();
@@ -21,8 +66,24 @@ public class UserRegistration {
 
     }
 
+    private void validPassword() {
+        System.out.println("Enter the password: ");
+        String password = scanner.nextLine();
+        String regex = ".{8,}";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(password);
+        boolean b = m.matches();
+        if (b)
+            System.out.println("password of the user is valid.");
+        else
+            System.out.println("password of the user is invalid.");
+
+    }
+
+
     public static void main(String[] args) {
         UserRegistration user = new UserRegistration();
-        user.validMobileNo();
+        user.validPassword();
     }
+
 }
